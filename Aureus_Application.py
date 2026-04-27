e29acf137c2506c364d5f95319a2fe1323413770
Aditya <arout2922@gmail.com>
2026-04-27 10:52:04 +0530

    User interface changes

--- Aureus_Application.py
+++ Aureus_Application.py
@@ -16,12 +16,30 @@ st.write("""Track every expense with ease and clarity.
 Stay informed about your spending in real time.""")
 
 st.write("---")
+st.subheader("Quick Navigation-")
+st.write("---")
 
-if st.button("➕ Add Expense"):
-    st.switch_page("pages/2_Add.py")
-if st.button("🔍 View Your Expense list"):
-    st.switch_page("pages/3_View.py")
-if st.button("📊 View Your Dashboard"):
-    st.switch_page("pages/1_Dashboard.py")
-if st.button("🧮 Open Calculator"):
-    st.switch_page("pages/4_QuickCalc.py")
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    if st.button("➕Add Expense"):
+        st.switch_page("pages/2_Add.py")
+with col2:
+    if st.button("🔍View Expense"):
+        st.switch_page("pages/3_View.py")
+with col3:        
+    if st.button("📊View Dashboard"):
+        st.switch_page("pages/1_Dashboard.py")
+with col4:
+    if st.button("🧮Open Calculator"):
+        st.switch_page("pages/4_QuickCalc.py")
+st.write("---")
+st.sidebar.markdown("### Version 1.1")
+st.sidebar.caption("Added QuickCalc")
+
+st.write("---")
+st.markdown("What's New in v1.1")
+st.error("""
+        - Added QuickCalc-Personal_buildin_Calculator.
+        - Supports basic arethemetic operations.
+        - Improve user interface.
+     """)
